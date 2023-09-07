3628948e670075427f621e6913f7ed6cd2df753d
Harshit Mishra <harshitlove28@outlook.com>
2023-09-08 00:39:25 +0530

    Adding Head component to every page

--- pages/contactus/index.js
+++ pages/contactus/index.js
@@ -1,10 +1,42 @@
 import NavbarComp from '@/components/NavbarComp'
 import ContactHeaderComp from '@/components/contactusPage/ContactHeaderComp'
 import React from 'react'
-
+import Head from 'next/head'
 function index() {
   return (
-      <div>
+    <div>
+      <Head>
+        <title>Contact Us</title>
+        <meta name="description" content="Office Address - 32, Shastri Nagar, Jaipur| info@ramrajassociates.com | +91-9116145123" />
+        <link rel="icon" href="/Images/favicon.png" />
+        <meta name="keywords" content="digital marketing services, digital marketing agency in india, digital marketing company in india, digital marketing services in india, digital marketing agency in delhi, digital marketing company in delhi, digital marketing services in delhi, digital marketing agency in noida, digital marketing company in noida, digital marketing services in noida, digital marketing agency in gurgaon, digital marketing company in gurgaon, digital marketing services in gurgaon, digital marketing agency in faridabad, digital marketing company in faridabad, digital marketing services in faridabad, digital marketing agency in ghaziabad, digital marketing company in ghaziabad, digital marketing services in jaipur" />
+      <meta property="og:title" content="Digital Marketing Services | Digital Marketing Agency in India" />
+      <meta property="og:description" content="We offer a wide range of digital services in India including Social Media Marketing, Search Engine Optimization, Email Marketing, Graphic Design, Content Writing, Content Marketing, Website Design & Development, Application Development, Paid Marketing, Corporate Photoshoot and Others." />
+      <meta property="og:url" content="https://www.ramrajassociates.com/contactus" />
+      {/* <meta property="og:image" content="https://www.ramrajassociates.com/Images/digital.jpg" /> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_IN" />
+      <meta property="og:site_name" content="Ramraj Associates" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@RamrajAssociate" />
+      <meta name="twitter:creator" content="@RamrajAssociate" />
+      <meta name="twitter:title" content="Digital Marketing Services | Digital Marketing Agency in India" />
+      <meta name="twitter:description" content="We offer a wide range of digital services in India including Social Media Marketing, Search Engine Optimization, Email Marketing, Graphic Design, Content Writing, Content Marketing, Website Design & Development, Application Development, Paid Marketing, Corporate Photoshoot and Others." />
+      {/* <meta name="twitter:image" content="https://www.ramrajassociates.com/Images/digital.jpg" /> */}
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="bingbot" content="index, follow" />
+      <meta name="slurp" content="index, follow" />
+      <meta name="rating" content="general" />
+      <meta name="referrer" content="always" />
+      <meta name="distribution" content="global" />
+      <meta name="coverage" content="Worldwide" />
+      <meta name="target" content="all" />
+      <meta name="audience" content="all" />
+      <meta name="revisit-after" content="7 days" />
+      <meta name="language" content="English" />
+        
+        </Head>
           <NavbarComp bgColor="black" position="sticky"/>
           <ContactHeaderComp/>
     </div>
--- pages/digital-marketing-services/index.js
+++ pages/digital-marketing-services/index.js
@@ -2,8 +2,39 @@ import NavbarComp from '@/components/NavbarComp'
 import React from 'react'
 import Image from 'next/image'
 import { Fade } from 'react-reveal'
+import Head from 'next/head'
 function index() {
-    return (<>
+  return (<>
+    <Head>
+      <title>Digital Marketing Services | Digital Marketing Agency in India</title>
+      <meta name="description" content="We offer a wide range of digital services in India including Social Media Marketing, Search Engine Optimization, Email Marketing, Graphic Design, Content Writing, Content Marketing, Website Design & Development, Application Development, Paid Marketing, Corporate Photoshoot and Others." />
+      <meta name="keywords" content="digital marketing services, digital marketing agency in india, digital marketing company in india, digital marketing services in india, digital marketing agency in delhi, digital marketing company in delhi, digital marketing services in delhi, digital marketing agency in noida, digital marketing company in noida, digital marketing services in noida, digital marketing agency in gurgaon, digital marketing company in gurgaon, digital marketing services in gurgaon, digital marketing agency in faridabad, digital marketing company in faridabad, digital marketing services in faridabad, digital marketing agency in ghaziabad, digital marketing company in ghaziabad, digital marketing services in jaipur" />
+      <meta property="og:title" content="Digital Marketing Services | Digital Marketing Agency in India" />
+      <meta property="og:description" content="We offer a wide range of digital services in India including Social Media Marketing, Search Engine Optimization, Email Marketing, Graphic Design, Content Writing, Content Marketing, Website Design & Development, Application Development, Paid Marketing, Corporate Photoshoot and Others." />
+      <meta property="og:url" content="https://www.ramrajassociates.com/digital-marketing-services" />
+      {/* <meta property="og:image" content="https://www.ramrajassociates.com/Images/digital.jpg" /> */}
+      <meta property="og:type" content="website" />
+      <meta property="og:locale" content="en_IN" />
+      <meta property="og:site_name" content="Ramraj Associates" />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@RamrajAssociate" />
+      <meta name="twitter:creator" content="@RamrajAssociate" />
+      <meta name="twitter:title" content="Digital Marketing Services | Digital Marketing Agency in India" />
+      <meta name="twitter:description" content="We offer a wide range of digital services in India including Social Media Marketing, Search Engine Optimization, Email Marketing, Graphic Design, Content Writing, Content Marketing, Website Design & Development, Application Development, Paid Marketing, Corporate Photoshoot and Others." />
+      {/* <meta name="twitter:image" content="https://www.ramrajassociates.com/Images/digital.jpg" /> */}
+      <meta name="robots" content="index, follow" />
+      <meta name="googlebot" content="index, follow" />
+      <meta name="bingbot" content="index, follow" />
+      <meta name="slurp" content="index, follow" />
+      <meta name="rating" content="general" />
+      <meta name="referrer" content="always" />
+      <meta name="distribution" content="global" />
+      <meta name="coverage" content="Worldwide" />
+      <meta name="target" content="all" />
+      <meta name="audience" content="all" />
+      <meta name="revisit-after" content="7 days" />
+      <meta name="language" content="English" />
+      </Head>
             <NavbarComp />
         <div >
             <div className='object-contain h-[100vh] relative !snap-center '>
@@ -85,7 +116,7 @@ function index() {
         <div class="bg-gray-100 p-6 rounded-lg">
           <Image class="h-40 rounded w-full object-cover object-center mb-6" src={'/Images/g-d.jpg'} alt="content" width={520} height={500}></Image>
           
-          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Graphic Desing</h2>
+          <h2 class="text-lg text-gray-900 font-medium title-font mb-4">Graphic Design</h2>
           <p class="leading-relaxed text-base"> Make a lasting impression with eye-catching visuals. Our talented graphic designers transform ideas into striking designs that communicate your brand's identity. From logos and branding materials to marketing collateral and digital graphics, we craft designs that leave a mark. Elevate your brand's aesthetics with our creative expertise in graphic design.</p>
         </div>
       </div>
