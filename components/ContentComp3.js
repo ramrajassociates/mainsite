26fee7d42dc85e010ebbab32b366d5a1ea6cf4af
Harshit Mishra <harshitlove28@outlook.com>
2023-10-25 23:58:49 +0530

    content component 3 card created

--- components/ContentComp3.js
+++ components/ContentComp3.js
@@ -5,6 +5,7 @@ function ContentComp3({content}) {
       <div className='flex flex-col space-y-3'>
           {content.map((item, index) => {
               return (<>
+                  <ContentComp3Card item={item} key={index} />
               </>)
           }
           )}
