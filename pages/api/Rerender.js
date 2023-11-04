c33f8afc1c662c14a6a962a336b05a462cb9c787
Harshit Mishra <harshitlove28@outlook.com>
2023-11-04 23:47:43 +0530

    api update for webhook

--- pages/api/Rerender.js
+++ pages/api/Rerender.js
@@ -1,14 +1,12 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-    console.log('webhook received', req.body)
-    // // if (req.body.model === 'blog') { 
-    //   const updatedEntry = req.body.entry.slug;
-    //   // console.log('updatedEntry', updatedEntry)
-    //   res.revalidate(`/Blogs/${updatedEntry}`);
-    // // } 
-  
-    //   res.status(500).json({ message: 'Something went wrong!' })
+  console.log('webhook received', req.body)
+  if (req.model.entry.URL) {
+
+      res.revalidate(`/${req.model.entry.URL }`);
+  }
+  res.json({message:"success"})
     
   }
   
