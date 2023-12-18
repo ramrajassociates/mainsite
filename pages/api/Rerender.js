dee221af97561dcf4b02966aa3e4423d2d1c7526
Harshit Mishra <harshitlove28@outlook.com>
2023-12-19 00:08:28 +0530

    Error wrapper in try catch in rerender

--- pages/api/Rerender.js
+++ pages/api/Rerender.js
@@ -1,12 +1,13 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  console.log('webhook received', req.body)
-  if (req.body.entry.URL) {
-
-      res.revalidate(`${req.body.entry.URL }`);
+  // console.log("webhook received", req.body);
+  try {
+    if (req.body.entry?.URL) {
+      res.revalidate(`${req.body.entry.URL}`);
+    }
+    res.json({ message: "success" });
+  } catch (error) {
+    res.json({ message: "Something went wrong" });
   }
-  res.json({message:"success"})
-    
-  }
-  
+}
